ae2b6aca3945c19e2ad0022b279c401e4073b1e3
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2026-04-01 11:37:10 +0300

    topic1

---
+++ topic1.js
@@ -0,0 +1,158 @@
+// alert("Hello World");
+
+// console.log("Hello World");
+
+// далі іде функція, яка виконується при кліку на заголовок
+
+// console.log("Hello World"); // це повідомлення виводиться в консоль, а не в сповіщенні
+
+// console.log("Hello World"); 
+// console.log("Hello World"); 
+// console.log("Hello World"); 
+
+// змінні
+// let const var
+
+// let name = "John";
+
+// name = "Jane";
+
+// name = 4;
+
+// const name = "John";
+
+// name = "Jane";
+
+
+// console.log(name);
+
+// типи даних
+
+// String
+// 'Hello World'
+
+// Number
+// 2
+// BigInt
+// 9007199254740991n
+
+// Boolean
+// true
+// false
+
+// Null
+
+// let name = 242;
+// name = null;
+
+// Undefined
+
+// let name;
+
+// console.log(name);
+
+// Symbol
+
+// Object
+
+// let person = {
+//     name: "John",
+//     age: 30,
+//     isStudent: true
+// };
+
+// let x = 5;
+
+// x = true;
+
+// x = "Hello World";
+
+// console.log(typeof x);
+
+
+// String
+
+
+// console.log("hello" + "world");
+
+// console.log(2 + "2");
+
+// console.log("2" / 1);
+
+// console.log(10 + "5")
+
+// операції з числами
+
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(10 * 5);
+// console.log(10 / 5);
+// console.log(10 % 3); //1
+
+// console.log(10 ** 2); //100
+
+// console.log(Math.pow(10,3))
+// console.log(Math.sqrt(4))
+
+// let x = 10;
+
+// x = x + 5;
+// x += 5; // x = x + 5
+// x -= 5; // x = x - 5
+// x *= 5;
+// x /= 5;
+// x++
+// x--
+
+// x += 1
+// x -= 1
+
+// x = 10;
+
+// console.log(x > 0 && x < 50)
+// console.log(x > 0 && x < 50)
+// console.log(x < 0 || x > 50)
+
+//тернарний оператор
+
+// let age = 18;
+// let isAdult = age >= 18 ? true : false;
+
+// alert("Hello World");
+
+// let name = prompt("What is your name?");
+// let age = prompt("How old are you?");
+
+// alert("Hello " + name + ", you are " + age + " years old.");
+
+// if else
+// switch case
+
+// let age = 35;
+
+// if (age >= 18) {
+//     alert("You are an adult.");
+// } else {
+//     alert("You are a minor.");
+// }
+
+// if (age >= 20 && age <= 30 ) {
+//     alert(" age is between 20 and 30");
+// } else if (age > 30 && age <= 40) {
+//     alert(" age is between 30 and 40");
+// } else {
+//     alert(" age is less than 20 or greater than 40");
+// }
+
+// let age = 15;
+
+// switch (age) {
+//     case 18:
+//         alert("You are 18 years old.");
+//         break;
+//     case 30:
+//         alert("You are 30 years old.");
+//         break;
+//     default:
+//         alert("Your age is " + age);
+// }
